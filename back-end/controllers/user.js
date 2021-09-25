82ddd05b25da637ced914b31cb7aa6f025897e51
Tihomir Shterev <tihomir.shterev.1@gmail.com>
2021-09-25 03:58:44 +0300

    Created authentication controllers and routes

---
+++ back-end/controllers/user.js
@@ -0,0 +1,56 @@
+const User = require('../models/User');
+const generateToken = require('../utils/generateToken');
+
+module.exports = {
+  register: async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+      const foundUser = await User.findOne({ email });
+
+      if (foundUser) {
+        res.status(400);
+        throw new Error('User already exists!');
+      }
+
+      const createdUser = await User.create({ email, password });
+
+      if (createdUser) {
+        const { _id, email } = createdUser;
+        res.status(201).json({
+          _id,
+          email,
+          token: generateToken(createdUser._id)
+        });
+      } else {
+        res.status(400);
+        throw new Error('Error occured!');
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  login: async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+      const foundUser = await User.findOne({ email });
+      const matchedPassword = await foundUser.matchPassword(password);
+
+      if (foundUser && matchedPassword) {
+        const { _id, email } = foundUser;
+        res.json({
+          _id,
+          email,
+          token: generateToken(foundUser._id)
+        });
+      } else {
+        res.status(400);
+        throw new Error('Invalid email or password!');
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  logout: async () => { }
+};
