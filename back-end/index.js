c7a7dbc304927a2caaaefb2acd2d7e8609ba413e
Tihomir Shterev <tihomir.shterev.1@gmail.com>
2021-09-25 03:57:52 +0300

    Updated index.js

--- back-end/index.js
+++ back-end/index.js
@@ -1,9 +1,13 @@
+const express = require('express');
+const { notFound, errorHandler } = require('./middlewares/error');
+const router = require('./routes');
+
 require('dotenv').config();
-
 require('./config/db')();
-
-const app = require('express')();
-
+const app = express();
+app.use(express.json());
+app.use('/api', router);
+app.use(notFound);
+app.use(errorHandler);
 const PORT = process.env.PORT || 3001;
-
 app.listen(PORT, console.log(`Server started on PORT ${PORT}`));
