b45219d60240dcff78509507df31bf539e194f74
Tihomir Shterev <tihomir.shterev.1@gmail.com>
2021-10-01 17:40:25 +0300

    Minor code structure and styling optimizations
    Exchanged div with main and added dynamic font-size

--- front-end/src/components/pages/search/index.js
+++ front-end/src/components/pages/search/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router';
+import { useDispatch, useSelector } from 'react-redux';
 import { searchByTitle } from '../../../redux/actions/movieActions';
 import Movie from '../../common/movie';
 import SearchBar from '../../common/searchBar';
@@ -10,34 +10,35 @@ const Search = () => {
   const { movieTitle } = useParams();
   const dispatch = useDispatch();
   const movies = useSelector(state => state.searchByTitle);
-  console.log(movies);
 
   useEffect(() => {
     dispatch(searchByTitle(movieTitle));
   }, [dispatch, movieTitle]);
 
   return (
-    <div className={styles["search-page"]}>
+    <main className={styles["search-page"]}>
       <h1>Search</h1>
-      <SearchBar />
-      {movieTitle && movies.length > 0 && (
-        <ul className={styles["movie-list"]}>
-          {movies.map(movie => <Movie movie={movie} />)}
-        </ul>
-      )}
-      {movieTitle && movies.length === 0 ? (
-        <p style={{ color: 'red' }}>
-          No results found. Please try again with a different title.
-        </p>
-      ) : (
-        <p style={{ color: 'green' }}>
-          Start searching for a movie.
-        </p>
-      )}
-      {movies.length === 0 && (
-        <img src="https://i.ibb.co/TtRbRP3/movie.jpg" alt="background-img" width={'100%'} />
-      )}
-    </div>
+      <section className={styles["search-section"]}>
+        <SearchBar />
+      </section>
+      <section className={styles["movies-section"]}>
+        {movieTitle && movies.length > 0 && movies.map(movie => {
+          return <Movie key={movie.id} movie={movie} />;
+        })}
+        {movieTitle && movies.length === 0 ? (
+          <p style={{ color: 'red' }}>
+            No results found. Please try again with a different title.
+          </p>
+        ) : (
+          <p style={{ color: 'green' }}>
+            Start searching for a movie.
+          </p>
+        )}
+        {movies.length === 0 && (
+          <img src="https://i.ibb.co/TtRbRP3/movie.jpg" alt="background-img" width={'100%'} />
+        )}
+      </section>
+    </main>
   );
 };
 
